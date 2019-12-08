ca07f4d5cdbae5720a1a94e1e38a6b5e8a13ed09
Wendy Kong <wendko.88@gmail.com>
2019-12-08 14:37:26 +0800

    Initial commit

---
+++ src/components/calendar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import moment from "moment";
+import "../styles.css";
+
+const getCalendarWeeks = (startDate, endDate) => {
+  const startDateMoment = moment(startDate);
+  const endDateMoment = moment(endDate);
+
+  const diffInWeeks = endDateMoment.diff(startDateMoment, "weeks");
+  if (!diffInWeeks) return;
+
+  let currentDate = startDateMoment;
+  const calendarWeeks = [];
+
+  while (currentDate < endDateMoment) {
+    calendarWeeks.push({
+      week: currentDate.week(),
+      displayDate: currentDate.format("DD MMM YYYY"),
+      isPast: currentDate < moment.now()
+    });
+    currentDate = currentDate.add(1, "week");
+  }
+
+  // const calendarWeeks2 = new Array(diffInWeeks).fill([0, 0]).map((_, i) => {
+  //   const currentDate = startDateMoment.add(+i, "week"); // something is wrong here
+  //   return [currentDate.week(), currentDate.format("DD MMM YYYY")];
+  // });
+
+  return calendarWeeks;
+};
+
+const pastDateStyle = {
+  textDecoration: "line-through"
+};
+
+const Calendar = () => {
+  const startDate = "2019-12-09";
+  const endDate = "2020-12-31";
+
+  const calendarWeeks = getCalendarWeeks(startDate, endDate);
+
+  return (
+    <div>
+      <p className="title"> Calendar Weeks </p>
+      <div className="date-list">
+        {calendarWeeks.map((c, i) => (
+          <div
+            key={i}
+            className="date-row"
+            style={c.isPast ? pastDateStyle : null}
+          >
+            <div>week {c.week}</div>
+            <div>{c.displayDate}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calendar;
